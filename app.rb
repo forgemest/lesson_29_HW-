9f874ba60f584a237793ddc9467bad3490047907
forgemes <forgemest@gmail.com>
2022-04-01 13:59:11 +0300

    add to db

--- app.rb
+++ app.rb
@@ -9,12 +9,29 @@ class Client < ActiveRecord::Base
 end
 
 
+# before do
+# 	@clients = client.all
+# end
+
 get '/' do
 	erb :index
 end
 
 get '/visit' do
 	erb :visit
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+
+
+client = Client.create(name: @username, phone: @phone, datestamp: @datetime, barber: @barber)
+
+erb :visit 
+
 end
 
 get '/contacts' do
