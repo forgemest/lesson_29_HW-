5df4e08dc37e1ae580627bdd96028dd1134c870e
forgemes <forgemest@gmail.com>
2022-04-04 18:21:10 +0300

    add error

--- app.rb
+++ app.rb
@@ -6,45 +6,33 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :datestamp, presence: true
+
 end
 
-
-# before do
-# 	@clients = client.all
-# end
-
-get '/' do
+get '/' do 
 	erb :index
 end
 
 get '/visit' do
+  @c = Client.new
 	erb :visit
 end
 
 
-
 post '/visit' do
-  c = Client.new params[:client]
-  c.save
+  @c = Client.new params[:client]
+  if @c.save
+erb "<h2>Спасибо вы записались</h2>"
+     else
+  # erb "<h2>Ошибка</h2>"
+  @error = @c.errors.full_messages.first
 erb :visit 
 
-end
-
-
-
-
-# post '/visit' do
-#   @username = params[:username]
-#   @phone = params[:phone]
-#   @datetime = params[:datetime]
-#   @barber = params[:barber]
-
-
-# client = Client.create(name: @username, phone: @phone, datestamp: @datetime, barber: @barber)
-
-# erb :visit 
-
-# end
+     end
+  end
 
 get '/contacts' do
 	erb :contacts
